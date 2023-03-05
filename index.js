7dcfdde10da07cc0f6838e5c33e08d45b8767048
maodas <rodas807@gmail.com>
2023-03-05 03:55:56 -0600

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+var formulario = document.getElementById('form-login')
+var nombre = document.getElementById('nombre-login')
+var email = document.getElementById('email-login')
+var fecha = document.getAnimations('fecha-nac')
+var password = document.getElementById('pass-login')
+
+var expMay = RegExp("[A-Z]")
+var expMin = RegExp("[a-z]")
+var expNum = RegExp("[0-9]")
+var exp
+
