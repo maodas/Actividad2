e908b52429b6a2838ab4d2d04791eebd2db517a6
maodas <rodas807@gmail.com>
2023-03-05 19:55:12 -0600

    .

--- index.js
+++ index.js
@@ -7,9 +7,45 @@ var password = document.getElementById('pass-login')
 var expMay = RegExp("[A-Z]")
 var expMin = RegExp("[a-z]")
 var expNum = RegExp("[0-9]")
-var exp
 
 formulario.addEventListener('submit',function(evento){
     evento.preventDefault()
-    alert("Bienvenido"+document.getElementById('pass-login').value)
+
+    var error=""
+
+    if(!nombre.value.match(expMay)){
+        error += "Su Nombre/Usuario debe tener una Mayuscula  "      
+    }if(!nombre.value.match(expMin)){
+        error += "Su Nombre/Usuario debe tener una Minuscula  "
+    }if(!nombre.value.match(expNum)){
+        error += "Su Nombre/Usuario debe tener un Numero  "
+    }if(!password.value.match(expMay)){
+        error += "Su Password debe tener una Mayuscula  "
+    }if(!password.value.match(expMin)){
+        error += "Su Password debe tener una Mayuscula  "
+    }if(!password.value.match(expNum)){
+        error += "Su Password debe tener un Numero  "
+    }
+    
+
+    alert(error)
 })
+
+
+
+
+/*formulario.addEventListener('submit',function(evento){
+    evento.preventDefault()
+
+    var error=""
+
+        if(!password.value.match(expMay)){
+            error += "Su Password debe tener una Mayuscula"
+        }else if(!password.value.match(expMin)){
+            error += "Su Password debe tener una Mayuscula"
+        }else if(!password.value.match(expNum)){
+            error += "Su Password debe tener un Numero"
+        }
+           
+        alert(error)
+    })*/
